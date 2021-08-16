f906eedbba878fd8c989b7402546fd39d7de4de8
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-15 22:38:39 -0500

    up and del post

--- controllers/api/post-routers.js
+++ controllers/api/post-routers.js
@@ -73,3 +73,65 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//Create Post
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        contents: req.body.contents,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Update Post
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            contents: req.body.contents
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Delete Post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
