07f3dc8bd1cf5454d7cbfd56267dc2271e86a4af
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-15 22:37:48 -0500

    get one post

--- controllers/api/post-routers.js
+++ controllers/api/post-routers.js
@@ -33,3 +33,43 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//Get one Post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id', 
+            'title', 
+            'contents', 
+            'user_id'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
