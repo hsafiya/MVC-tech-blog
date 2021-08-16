16782edfa37e5a3b77363412c0b53f09e0550dad
Safiyya H <safiyya.hasanova@gmail.com>
2021-08-15 22:34:57 -0500

    user routes new

--- controllers/api/user-routers.js
+++ controllers/api/user-routers.js
@@ -13,3 +13,60 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  //specific User
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'contents', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        }
+      ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// new User
+router.post('/', (req, res) => {
+    // expects {username: 'irynashulim', password: 'abc123'}
+    User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
